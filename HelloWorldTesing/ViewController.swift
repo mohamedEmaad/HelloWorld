cb14d2cee1392a3cbdb48a8c59b9252a748ca70f
Mohamed Emad <mohammed.emaad94@gmail.com>
2019-03-10 16:05:22 +0200

    some updates

--- HelloWorldTesing/ViewController.swift
+++ HelloWorldTesing/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var centerLabel: UILabel = {
+        let uiLable = UILabel()
+        uiLable.text = "Testing this app man!"
+        uiLable.translatesAutoresizingMaskIntoConstraints = false
+        return uiLable
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(self.centerLabel)
+        self.centerLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.centerLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
 
 
